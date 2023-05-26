5ec6e14c03b8cc8c5edc2bccad459410faae32c5
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-05-26 16:23:28 -0500

    Last changes

---
+++ test/main_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+//Imports the main.dart file
+import '../lib/main.dart';
+
+void main() {
+  testWidgets('MyApp renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    final titleFinder = find.text('Flutter App');
+    final textFinder = find.text('Hello, World!');
+
+    expect(titleFinder, findsOneWidget);
+    expect(textFinder, findsOneWidget);
+  });
+}
