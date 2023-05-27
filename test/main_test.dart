f8a1c07906166438d7604126b82c95f2f568c298
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-05-26 21:53:43 -0500

    Error

--- test/main_test.dart
+++ test/main_test.dart
@@ -6,7 +6,7 @@ void main() {
   testWidgets('MyApp renders correctly', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
     //Sasdas
-    final titleFinder = find.text('Flutter App');
+    final titleFinder = find.text('Flutter Ap');
     final textFinder = find.text('Hello, World');
 
     expect(titleFinder, findsOneWidget);
